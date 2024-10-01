435024906022c78eec47fd7eaec0d24af58ae044
palva39 <paa39@njit.edu>
2024-10-01 19:34:22 -0400

    updated pyest with two tests for each operation

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,37 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+import pytest
+from calculator import Calculator
 
 def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+    assert Calculator.add(2, 3) == 5
+    assert Calculator.add(-1, -1) == -2
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
-    
+    assert Calculator.subtract(2, 3) == -1
+    assert Calculator.subtract(-1, -1) == 0
+
+def test_multiplication():
+    assert Calculator.multiply(2, 3) == 6
+    assert Calculator.multiply(-1, -1) == 1
+
+def test_division():
+    assert Calculator.divide(10, 2) == 5
+    assert Calculator.divide(-1, -1) == 1
+
+def test_divide_by_zero_exception():
+    with pytest.raises(ValueError):
+        Calculator.divide(10, 0)
+
+def test_last_calculation():
+    Calculator.add(5, 7)
+    assert Calculator.last_calculation() == (5, 7, '+', 12)
+
+@pytest.fixture
+def clean_history():
+    Calculator.history.clear()
+
+def test_history(clean_history):
+    Calculator.add(2, 2)
+    assert Calculator.last_calculation() == (2, 2, '+', 4)
+    Calculator.subtract(3, 2)
+    assert Calculator.last_calculation() == (3, 2, '-', 1)
