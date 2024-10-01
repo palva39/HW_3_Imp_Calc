0dfe32c06aa732fd78d1562d2b894a3ff0702cdc
palva39 <paa39@njit.edu>
2024-10-01 18:42:33 -0400

    adjusted files

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,32 @@
-def add(a,b):
-    return a + b
+class Calculator:
+    history = []
 
-def subtract(a,b):
-    return a - b
+    @staticmethod
+    def add(a: int, b: int) -> int:
+        result = a + b
+        Calculator.history.append((a, b, '+', result))
+        return result
+
+    @staticmethod
+    def subtract(a: int, b: int) -> int:
+        result = a - b
+        Calculator.history.append((a, b, '-', result))
+        return result
+
+    @staticmethod
+    def multiply(a: int, b: int) -> int:
+        result = a * b
+        Calculator.history.append((a, b, '*', result))
+        return result
+
+    @staticmethod
+    def divide(a: int, b: int) -> int:
+        if b == 0:
+            raise ValueError("Cannot divide by zero.")
+        result = a / b
+        Calculator.history.append((a, b, '/', result))
+        return result
+
+    @classmethod
+    def last_calculation(cls):
+        return cls.history[-1] if cls.history else None
