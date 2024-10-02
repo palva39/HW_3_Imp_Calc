5d51b7f9d07241d852d89a337a2838721b2b1215
palva39 <paa39@njit.edu>
2024-10-01 21:22:13 -0400

    Fix pylint issues in test_calculator.py by adding docstrings and adjusting linting rules for tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,34 +3,42 @@ import pytest
 from calculator import Calculator
 
 def test_addition():
+    '''Tests the addition function'''
     assert Calculator.add(2, 3) == 5
     assert Calculator.add(-1, -1) == -2
 
 def test_subtraction():
+    '''Tests the subtraction function'''
     assert Calculator.subtract(2, 3) == -1
     assert Calculator.subtract(-1, -1) == 0
 
 def test_multiplication():
+    '''Tests that multiplcation function'''
     assert Calculator.multiply(2, 3) == 6
     assert Calculator.multiply(-1, -1) == 1
 
 def test_division():
+    '''Tests the division function'''
     assert Calculator.divide(10, 2) == 5
     assert Calculator.divide(-1, -1) == 1
 
 def test_divide_by_zero_exception():
+    '''Tests the addition function when diveded by 0'''
     with pytest.raises(ValueError):
         Calculator.divide(10, 0)
 
 def test_last_calculation():
+    '''Tests the last calculation done function'''
     Calculator.add(5, 7)
     assert Calculator.last_calculation() == (5, 7, '+', 12)
 
 @pytest.fixture
 def clean_history():
+    '''Tests the clean history'''
     Calculator.history.clear()
 
 def test_history(clean_history):
+    '''Test the calculation history functionality.'''
     Calculator.add(2, 2)
     assert Calculator.last_calculation() == (2, 2, '+', 4)
     Calculator.subtract(3, 2)
